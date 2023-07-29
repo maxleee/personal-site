f42649c66e11515d111e2e5a26ae70092664e33d
Max <max.schwanger@gmail.com>
2023-07-29 12:17:34 -0400

    font change

--- components/layout.js
+++ components/layout.js
@@ -11,12 +11,17 @@ export default function Layout({ children, home }) {
     <div>
       <Head>
         <meta name='description' content='Max Orenzuk, a designer and front end developer' />
-        <meta name='description' content='Max Orenzuk, a designer and front end developer' />
         <meta name='og:title' content={siteTitle} />
         <meta name='twitter:card' content='summary_large_image' />
         <link
           rel='icon'
           href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧑‍💻</text></svg>'
+        />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+        <link
+          href='https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,400;9..40,500;9..40,600;9..40,700&display=swap'
+          rel='stylesheet'
         />
       </Head>
       <MotionConfig reducedMotion='user'>
