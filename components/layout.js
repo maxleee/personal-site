a1a60958ad79a2c7f7cb8158d58cd29fac89900d
Max <max.schwanger@gmail.com>
2023-07-29 12:28:08 -0400

    fonts in document

--- components/layout.js
+++ components/layout.js
@@ -17,12 +17,6 @@ export default function Layout({ children, home }) {
           rel='icon'
           href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧑‍💻</text></svg>'
         />
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
-        <link
-          href='https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,400;9..40,500;9..40,600;9..40,700&display=swap'
-          rel='stylesheet'
-        />
       </Head>
       <MotionConfig reducedMotion='user'>
         {home ? (
