25691bb8bc6f56dc361fc6a3808d42b6027d4ff9
Max <max.schwanger@gmail.com>
2023-08-13 11:55:03 -0400

    dynamic routing with contentlayer

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,11 +1,12 @@
 import Layout from '../../components/layout';
-import { getAllPostIds, getPostData } from '../../lib/posts';
+// import { getAllPostIds, getPostData } from '../../lib/posts';
+import { allPosts } from 'contentlayer/generated';
 import Head from 'next/head';
 import ExternalLinkIcon from '../../components/externalLinkIcon';
 import TableList from '../../components/tableList';
 
 export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id);
+  const postData = allPosts.find(post => post._raw.flattenedPath === params.id);
   return {
     props: {
       postData,
@@ -14,7 +15,8 @@ export async function getStaticProps({ params }) {
 }
 
 export async function getStaticPaths() {
-  const paths = getAllPostIds();
+  const paths = allPosts.map(post => ({ params: { id: post._raw.flattenedPath } }));
+
   return {
     paths,
     fallback: false,
